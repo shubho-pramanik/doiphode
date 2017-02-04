0eeb0f09c49be6e29bf5f5e85bb7f92751ebe336
shubho-acc <shubho@mail-acc.com>
2017-02-04 09:55:28 +0530

    changed to php

---
+++ home.php
@@ -0,0 +1,261 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+
+
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<title>मुख्य पान</title>
+
+<script type="text/javascript" src="scripts/jquery.js"></script>
+
+<script type="text/javascript" src="scripts/supersleight-min.js"></script>
+
+<SCRIPT type="text/javascript" src="scripts/jquery.js"></SCRIPT>
+
+
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<link rel="shortcut icon" type="image/x-icon" href="css/images/favicon.ico" />
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+	<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="all" />
+	
+	<script src="js/jquery-1.7.2.min.js" type="text/javascript"></script>
+	<!--[if lt IE 9]>
+		<script src="js/modernizr.custom.js"></script>
+	<![endif]-->
+	<script src="js/jquery.flexslider-min.js" type="text/javascript"></script>
+	<script src="js/functions.js" type="text/javascript"></script>
+
+
+<link rel="stylesheet" type="text/css" href="styles/stylesheet.css" />
+<link rel="stylesheet" type="text/css" href="styles/pde.css" />
+<SCRIPT type="text/javascript" src="scripts/pde.js"></SCRIPT>
+
+ 
+
+
+
+<link rel="stylesheet" type="text/css" href="styles/stylesheet.css" />
+
+<link rel="stylesheet" type="text/css" href="styles/pde.css" />
+
+<SCRIPT type="text/javascript" src="scripts/pde.js"></SCRIPT>
+
+
+
+<!--[if lte IE 6]>
+
+<script type="text/javascript" src="js/supersleight-min.js"></script>
+
+<![endif]-->
+
+
+
+
+
+
+
+</head>
+
+
+
+<body>
+
+
+
+<table width="888" border="0" align="center" cellpadding="0" cellspacing="0" id="wraper">
+
+  <tr>
+
+    <td>
+
+			<img src="images/BG-Header2.jpg" width="100%" height="80%"/> 			
+
+       </td>
+
+
+
+  </tr>
+
+  <tr>
+        <td colspan="2"><img src="images/Menu-HLine.jpg" alt="" width="888" height="1" /></td>
+        </tr>  
+
+  <tr>
+
+    <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+
+      <tr>
+
+        <td width="15"><img src="images/BG-MenuLeft.png" alt="" width="15" height="32" /></td>
+
+       <td class="nav" align="center">
+
+	<ul id="nav">
+
+		<li style=" width:20%";><a href="home.php" class=""><font  color="white"   size="4">मुख्य पान</font></a></li>
+
+		 <li class='line'><li style=" width:20%"; ><a href="aboutus.php"><font  color="white"   size="4">प्रास्ताविक</font></a></li>
+
+		 <li class='line'><li style=" width:15%";><a href="committee.php"><font  color="white"   size="4">समिती</font></a></li>
+
+		 <li class='line'><li style=" width:20%";><a href="gallery_Adsul_2011.php"><font  color="white"   size="4">छायाचित्रे</font></a></li>
+
+		 <li class='line'><li style=" width:20%";><a href="registration.php"><font  color="white"   size="4">सभासद नोंदणी</font></a></li>
+
+		
+
+	</ul>
+
+	
+
+ </td>
+
+
+
+        <td width="15"><img src="images/BG-MenuRight.png" alt="" width="15" height="32" /></td>
+
+      </tr>
+
+    </table></td>
+
+  </tr>
+
+  <tr>
+
+    <td height="2"></td>
+
+  </tr>
+
+ 
+
+   
+
+  <tr>
+
+    <td height="4"></td>
+
+  </tr>
+
+  <tr>
+
+    <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+
+	<!-- <tr>
+
+		<td height="78px">
+
+		</td>
+
+	 </tr> -->
+
+	 <tr>
+
+		<td>
+
+		
+
+		<!-- Baba -->
+
+		<div align="center">
+
+					
+
+                    <strong> <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" 
+
+                        codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,19,0" 
+
+                        width="460" height="335" style="margin-left: 0px">
+
+            <param name="movie" value="images/logo.swf" />
+
+            <param name="quality" value="high" />
+
+        <embed src="images/logo.swf" quality="high" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" width="460" height="335"></embed>  <!--width="460" height="436" -->   
+
+          </object></strong>
+
+					</div>
+
+		</td>
+
+	 </tr>
+
+ <tr>
+
+        <td><img src="images/BG-TopInnerContent.png" alt="" width="888" height="11" /></td>
+
+      </tr>
+
+      <tr>	 
+
+      <tr>
+
+        <td><table width="100%" border="0" cellspacing="0" cellpadding="0" id="innerContent">
+
+		
+
+          <tr>
+
+            <td width="225" valign="top">
+
+			 <table width="100%" border="0" cellspacing="0" cellpadding="0">
+
+
+
+                                    <tr><td valign="top">
+
+									
+
+									
+
+			 <img src="images/mainpage_bottom.jpg" alt="" width="100%" height="60%" />
+
+</td>
+
+      </tr>
+
+      <tr>
+
+        <td id="footer">
+
+		<a href="home.php" class="">मुख्य पान</a> 
+
+		<a href="aboutus.php">प्रास्ताविक</a>
+
+		<a href="committee.php">समिती</a>
+
+		<a href="gallery_Adsul_2011.php">छायाचित्रे</a>
+
+		<a href="registration.php">सभासद नोंदणी</a></td>
+
+      </tr>
+
+    </table></td>
+
+  </tr>
+
+  <tr>
+
+     <td height="25" class="bottom">copyright &copy; 2013 Samastha Doiphode. All rights reserved.</td>
+
+  </tr>
+
+</table>
+
+</td>
+
+</tr>
+
+</table>
+
+</body>
+
+</html>
